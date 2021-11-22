29d275754b03280a62a6e1454c4643354bbcb03b
BryanEmond <emond.bryan22@outlook.com>
2021-11-21 23:28:39 -0500

    test

--- src/main/java/codes/blitz/game/Solver.java
+++ src/main/java/codes/blitz/game/Solver.java
@@ -2,6 +2,8 @@ package codes.blitz.game;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.json.JsonMapper;
@@ -17,20 +19,18 @@ import codes.blitz.game.serialization.JsonMapperSingleton;
 
 public class Solver {
   private final JsonMapper jsonMapper = JsonMapperSingleton.getInstance();
-  private final ArrayList<Integer> matrix;
-  private int[][] I, L, J, T, Z, S, O;
+  private List<CoordinatePair> I, L, J, T, Z, S, O;
 
   public Solver() {
     // This method should be use to initialize some variables you will need
     // throughout the challenge.
-    this.matrix = new ArrayList<Integer>();
-    I = new int[][] { { 1, 1, 1, 1 } };
-    L = new int[][] { { 2, 0 }, { 2, 0 }, { 2, 2 } };
-    J = new int[][] { { 0, 3 }, { 0, 3 }, { 3, 3 } };
-    T = new int[][] { { 0, 4, 0 }, { 4, 4, 4 } };
-    Z = new int[][] { { 5, 5, 0 }, { 0, 5, 5 } };
-    S = new int[][] { { 0, 6, 6 }, { 6, 6, 0 } };
-    O = new int[][] { { 7, 7 }, { 7, 7 } };
+    I = List.of(new CoordinatePair(0, 0), new CoordinatePair(1, 0), new CoordinatePair(2, 0), new CoordinatePair(3, 0));
+    L = List.of(new CoordinatePair(0, 2), new CoordinatePair(0, 1), new CoordinatePair(0, 0), new CoordinatePair(1, 0));
+    J = List.of(new CoordinatePair(0, 1), new CoordinatePair(0, 0), new CoordinatePair(1, 0), new CoordinatePair(0, 2));
+    T = List.of(new CoordinatePair(0, 0), new CoordinatePair(1, 0), new CoordinatePair(2, 0), new CoordinatePair(1, 1));
+    Z = List.of(new CoordinatePair(0, 1), new CoordinatePair(1, 1), new CoordinatePair(1, 0), new CoordinatePair(2, 0));
+    S = List.of(new CoordinatePair(0, 0), new CoordinatePair(0, 1), new CoordinatePair(1, 1), new CoordinatePair(2, 1));
+    O = List.of(new CoordinatePair(1, 0), new CoordinatePair(1, 1), new CoordinatePair(0, 0), new CoordinatePair(1, 0));
 
   }
 
@@ -40,65 +40,88 @@ public class Solver {
    */
   public Answer getAnswer(GameMessage gameMessage) throws JsonProcessingException {
     Question question = gameMessage.payload();
-    // System.out.println("Received Question: " +
-    // jsonMapper.writeValueAsString(question));
-    var totems = List.of(new TotemAnswer(Totem.I, List.of(new CoordinatePair(0, 0), new CoordinatePair(1, 0),
-        new CoordinatePair(2, 0), new CoordinatePair(3, 0))));
-    placeTotems(question);
+    System.out.println("Received Question: " + jsonMapper.writeValueAsString(question));
+    // var totems = List.of(new TotemAnswer(Totem.I, List.of(new CoordinatePair(0,
+    // 0), new CoordinatePair(1, 0),
+    // new CoordinatePair(2, 0), new CoordinatePair(3, 0))));
+    var totems = placeTotems(question);
     Answer answer = new Answer(totems);
     System.out.println("Sending Answer: " + jsonMapper.writeValueAsString(answer));
+    System.out.println(answer);
     return answer;
   }
 
-  public List<Object> placeTotems(Question question) {
-    int[][] currentTotems;
-    var totems = List.of();
+  public List<TotemAnswer> placeTotems(Question question) {
+    List<CoordinatePair> currentTotems;
+    List<TotemAnswer> totems = new ArrayList<TotemAnswer>();
+    Totem totemForm;
+    int height = 0;
     for (int i = 0; question.totems().size() > i; i++) {
       switch (question.totems().get(i).shape().toString().toUpperCase()) {
       case "I":
         currentTotems = this.I;
+        totemForm = Totem.I;
         break;
       case "L":
         currentTotems = this.L;
+        totemForm = Totem.L;
         break;
       case "J":
         currentTotems = this.J;
+        totemForm = Totem.J;
         break;
       case "Z":
         currentTotems = this.Z;
+        totemForm = Totem.Z;
         break;
       case "S":
         currentTotems = this.S;
+        totemForm = Totem.S;
         break;
       case "T":
         currentTotems = this.T;
+        totemForm = Totem.T;
         break;
       case "O":
         currentTotems = this.O;
+        totemForm = Totem.O;
         break;
       default:
-        currentTotems = new int[][] { { 0, 0, 0, 0 } };
+        currentTotems = List.of(new CoordinatePair(0, 0), new CoordinatePair(0, 0), new CoordinatePair(0, 0),
+            new CoordinatePair(0, 0));
+        totemForm = Totem.O;
       }
 
-      if (currentTotems == this.I) {
-        for (int j = 0; currentTotems.length > j; j++) {
-          for (int x = 0; currentTotems[j].length > x; x++) {
-            if(currentTotems[j][x] != 0){
-              matrix.add(i + 1);
-            }else{
-              matrix.add(0);
-            }
-          }
-        }
-      } 
-      else {
-        // for (int j = 0; currentTotems.length > j; j++) {
-        //   for (int x = 0; currentTotems[j].length > x; x++) {
-        //     matrix.add(currentTotems[j]);
-        //   }
-        // }
+      currentTotems = currentTotems.stream().map(cord -> new CoordinatePair(cord.x(), cord.y() + height)).collect(Collectors.toList());
+
+      totems.add(new TotemAnswer(totemForm, currentTotems));
+      
+      switch (question.totems().get(i).shape().toString().toUpperCase()) {
+      case "I":
+        height += 1;
+        break;
+      case "L":
+        height += 3;
+        break;
+      case "J":
+        height += 2;
+        break;
+      case "Z":
+        height += 2;
+        break;
+      case "S":
+        height += 2;
+        break;
+      case "T":
+        height += 2;
+        break;
+      case "O":
+        height += 2;
+        break;
+      default:
+        height += 0;
       }
     }
-  // totems.add(new TotemAnswer(totemForm, List.of(pair1, pair2, pair3, pair4)));
-  return totems;
-}}
+    return totems;
+  }
+}
--- src/main/java/codes/blitz/game/message/Answer.java
+++ src/main/java/codes/blitz/game/message/Answer.java
@@ -1,6 +1,44 @@
 package codes.blitz.game.message;
 
 import java.util.List;
+import java.util.BitSet;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public record Answer(List<TotemAnswer> totems) {
+    @Override
+    public String toString() {
+        List<CoordinatePair> coordinates =
+            totems.stream().map(TotemAnswer::coordinates).flatMap(Collection::stream).collect(Collectors.toList());
+        int maxX = coordinates.stream().mapToInt(CoordinatePair::x).max().getAsInt()+1;
+
+        Map<Integer, List<BitSet>> collect = coordinates
+            .stream()
+            .collect(
+                Collectors.groupingBy(
+                    CoordinatePair::y,
+                    Collectors.mapping(
+                        coord -> {
+                            BitSet sb = new BitSet();
+                            sb.set(coord.x());
+                            return sb;
+                        },
+                        Collectors.toList())));
+
+        return collect
+            .entrySet()
+            .stream()
+            .sorted(Comparator.comparingInt(Map.Entry::getKey))
+            .map(entry-> entry.getValue().stream().reduce(new BitSet(maxX), (a, b) -> {
+                    a.or(b);
+                    return a;
+                }))
+            .map(bs-> IntStream.range(0, bs.length())
+                .mapToObj(b -> String.valueOf(bs.get(b) ? '*' : ' '))
+                .collect(Collectors.joining()))
+            .collect(Collectors.joining("\n"));
+    }   
 }
